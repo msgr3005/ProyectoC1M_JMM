9384c0de0ff4712fe7530e34e47e7a63ea62f8dc
Mauricio <85137746+msgr3005@users.noreply.github.com>
2023-05-30 17:49:07 -0500

    Primer avance de la interfaz

---
+++ src/avance/Avance.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package avance;
+
+/**
+ *
+ * @author MAURICIO
+ */
+public class Avance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginForm loginForm= new LoginForm();
+        loginForm.setVisible(true);
+        
+    }
+    
+}
