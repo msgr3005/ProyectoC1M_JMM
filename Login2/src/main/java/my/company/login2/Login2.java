f7b60f8cacd89d3540f3987e863559c091179289
Jhan Franco <84046499+Jhanfranco07@users.noreply.github.com>
2023-05-31 23:22:20 -0500

    Add files via upload

---
+++ Login2/src/main/java/my/company/login2/Login2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package my.company.login2;
+
+/**
+ *
+ * @author GIAN FRANCO
+ */
+public class Login2 {
+  
+    public static void main(String[] args) {
+        NewJFrame loginFrame = new NewJFrame();
+        loginFrame.setVisible(true);
+    }
+}
